9fc245ecac7639ad15b28c3ec81f9b72721b3457
turbomoai <turbomoai@github.com>
2025-04-15 21:28:27 +0200

    Ajoute la méthode POST au contrôleur

--- src/main/java/com/carproject/api/controller/CarController.java
+++ src/main/java/com/carproject/api/controller/CarController.java
@@ -3,10 +3,9 @@ package com.carproject.api.controller;
 import com.carproject.api.model.Car;
 import com.carproject.api.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -27,9 +26,9 @@ public class CarController {
         return carService.getCar(id);
     }
 
-    /*@PostMapping
+    @PostMapping
     public ResponseEntity<Car> save(@RequestBody Car car) {
         Car newCar = carService.save(car);
         return new ResponseEntity<>(newCar, HttpStatus.CREATED);
-    }*/
+    }
 }
